d60c399e72f8ecc20496e508d8ae32053255c007
Avinash S <avisanbiz@gmail.com>
2018-09-26 22:56:17 -0400

    Adding vim changes

--- vim/getplugins.sh
+++ vim/getplugins.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-if [ ! -d "$HOME/.vim/colors" ]; then
-    mkdir -p "$HOME/.vim/colors"
+VIM_DIR="$HOME/.vim"
+COLORS_DIR="$VIM_DIR/colors"
+BUNDLE_DIR="$VIM_DIR/bundle"
+AUTOLOAD_DIR="$VIM_DIR/autoload"
+
+if [ ! -d "$COLORS_DIR" ]; then
+    mkdir -p "$COLORS_DIR"
 fi
 
-if [ ! -d "$HOME/.vim/bundle" ]; then
-    mkdir -p "$HOME/.vim/bundle"
+if [ ! -d "$BUNDLE_DIR" ]; then
+    mkdir -p "$BUNDLE_DIR"
 fi
 
-if [ ! -d "$HOME/.vim/autoload" ]; then
-    mkdir -p "$HOME/.vim/autoload"
+if [ ! -d "$AUTOLOAD_DIR" ]; then
+    mkdir -p "$AUTOLOAD_DIR"
 fi
 
 # Vundle for vim
@@ -22,3 +27,6 @@ fi
 
 # https://github.com/tpope/vim-vinegar
 # https://github.com/Yggdroot/LeaderF
+
+# Get the colors
+wget https://raw.githubusercontent.com/dracula/vim/master/colors/dracula.vim -O $COLORS_DIR/dracula.vim
